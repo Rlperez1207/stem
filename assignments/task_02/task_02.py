bb3a5a2940f1c6840881dbeb8ffb3a1db1297a51
student <student>
2018-02-15 11:54:53 -0500

    i did work

--- assignments/task_02/task_02.py
+++ assignments/task_02/task_02.py
@@ -1,6 +1,10 @@
 # Write a function called total that takes a filename as its only argument and returns the sum of the numbers in that file.
 # You may assume that the file is formatted in such a way that each number is an integer and occupies its own line.
 # You may not assume you know how many numbers are in the file.
+def total(filename):
+    with open('numbers.txt', 'r') as f:
+        read_file = f.read()
+        return read_file
 
 <<<<<<< HEAD
 # The file numbers.txt in this directory follows these guidelines.
@@ -8,3 +12,4 @@
 # The file numbers.txt in this directory follows these guidelines and should add up to 4753817
 # After you've written your function, you can run test.py to ensure your function works on other files as well.
 >>>>>>> 7876bd454093c333da5c43708f4444ec994578e5
+dasdsads
